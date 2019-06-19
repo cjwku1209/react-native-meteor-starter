8603d950ef95a41db4214cbe3416b8163c74a12d
Christopher Wong <christopherpk1996@gmail.com>
2019-06-19 19:20:19 +0800

    rebuild redux

--- src/redux/sagas/logger-saga.js
+++ src/redux/sagas/logger-saga.js
@@ -3,9 +3,9 @@ import { LoggerAction } from "../actions/logger-action";
 
 export const LoggerSaga = function* () {
 	yield takeEvery('*', function* (action) {
-		if (action['type'] !== LoggerAction.WRITE_COMPLETE) {
+		if (action.type !== LoggerAction.WRITE_COMPLETE) {
 			console.log(action);
-			yield put(LoggerAction._WRITE_COMPLETE(action));
+			yield put(LoggerAction.log(action));
 		}
 	});
 };

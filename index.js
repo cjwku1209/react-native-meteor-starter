f7f6ac885de8e200558a0268c5432589f0c2e6ed
Christopher Wong <christopherpk1996@gmail.com>
2019-09-06 16:18:21 +0800

    init react-native 0.60.5

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import { AppRegistry } from 'react-native';
-import Meteor from 'react-native-meteor';
-import { name as appName } from './app.json';
-import { App } from "./src/App";
+/**
+ * @format
+ */
 
-AppRegistry.registerComponent(appName, () => {
-	Meteor.connect('ws://10.0.2.2:3000/websocket');
-	return App;
-});
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
+
+AppRegistry.registerComponent(appName, () => App);
--- src/redux/logger/logger-saga.js
+++
@@ -1,7 +0,0 @@
-import { takeEvery } from "redux-saga/effects";
-
-export const LoggerSaga = function* () {
-	yield takeEvery("*", function* (action) {
-		console.log(action);
-	});
-};
